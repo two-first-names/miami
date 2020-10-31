0c827ad946e72fd196ec6fe5b4862f612deca352
Joe Roberts <jroberts@jroberts.me.uk>
2020-10-31 15:29:58 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+REPO="https://github.com/two-first-names/miami"
+
+if [ ! -d "miami" ]
+then
+  git clone $REPO miami
+fi
+
+cd miami || exit
+git pull
+sudo pip install -r requirements.txt
+evennia start
