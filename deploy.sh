c8ef7f8f2b3c20e01681a579927274649484d899
Joe Roberts <jroberts@jroberts.me.uk>
2020-10-31 15:41:29 +0000

    Use virtualenv

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 REPO="https://github.com/two-first-names/miami"
 
+if [ ! -d "venv" ]
+then
+  python3 -mvenv venv
+fi
+source ./venv/bin/activate
+
 if [ ! -d "miami" ]
 then
   git clone $REPO miami
@@ -8,5 +14,5 @@ fi
 
 cd miami || exit
 git pull
-sudo pip install -r requirements.txt
+pip install -r requirements.txt
 evennia start
